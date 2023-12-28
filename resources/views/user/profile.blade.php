10728130f317a2e9c03027882faadcbe45ce87d7
alfaqosry <alfa.qosry123@gmail.com>
2023-12-28 20:18:37 +0700

    first commit

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -19,24 +19,24 @@
                                 <img src="{{ asset('sneat/assets/img/avatars/1.png') }}" alt="user-avatar"
                                     class="d-block rounded" height="100" width="100" id="uploadedAvatar" />
                             @else
-                                <img src="{{ Storage::url(auth()->user()->foto) }}" @endif
-                                alt="user-avatar"
-                                class="d-block rounded" height="100" width="100" id="uploadedAvatar" />
-                                <div class="button-wrapper">
+                                <img src="{{ Storage::url(auth()->user()->foto) }}" alt="user-avatar"
+                                    class="d-block rounded" height="100" width="100" id="uploadedAvatar" />
+                            @endif
+                            <div class="button-wrapper">
 
-                                    <label for="upload" class="btn btn-primary me-2 mb-4" tabindex="0">
-                                        <span class="d-none d-sm-block">Upload new photo</span>
-                                        <i class="bx bx-upload d-block d-sm-none"></i>
-                                        <input type="file" id="upload" class="account-file-input" name="foto"
-                                            hidden accept="image/png, image/jpeg" />
-                                    </label>
-                                    <button type="button" class="btn btn-outline-secondary account-image-reset mb-4">
-                                        <i class="bx bx-reset d-block d-sm-none"></i>
-                                        <span class="d-none d-sm-block">Reset</span>
-                                    </button>
+                                <label for="upload" class="btn btn-primary me-2 mb-4" tabindex="0">
+                                    <span class="d-none d-sm-block">Upload new photo</span>
+                                    <i class="bx bx-upload d-block d-sm-none"></i>
+                                    <input type="file" id="upload" class="account-file-input" name="foto" hidden
+                                        accept="image/png, image/jpeg" />
+                                </label>
+                                <button type="button" class="btn btn-outline-secondary account-image-reset mb-4">
+                                    <i class="bx bx-reset d-block d-sm-none"></i>
+                                    <span class="d-none d-sm-block">Reset</span>
+                                </button>
 
-                                    <p class="text-muted mb-0">Allowed JPG, GIF or PNG. Max size of 800K</p>
-                                </div>
+                                <p class="text-muted mb-0">Allowed JPG, GIF or PNG. Max size of 800K</p>
+                            </div>
                         </div>
                     </div>
                     <hr class="my-0" />
